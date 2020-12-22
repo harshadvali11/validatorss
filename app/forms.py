0a7d7b90ab86b8cc9407cdad3179f396af72e0e9
harshad116 <harshadvali1154@gmail.com>
2020-12-22 08:27:44 -0800

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.core import validators
 
 def check_for_a(value):
     if value[0].lower()!='a':
@@ -8,4 +8,20 @@ def check_for_a(value):
 
 
 class Student(forms.Form):
-    name=forms.CharField(max_length=100,validators=[check_for_a])
+    name=forms.CharField(max_length=100,validators=[check_for_a,validators.MaxLengthValidator(4)])
+    email=forms.EmailField(max_length=100)
+    reenter_email=forms.EmailField(max_length=100)
+    phone=forms.CharField(min_length=10,max_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
+    botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+
+    def clean_botcatcher(self):
+        bot=self.cleaned_data.get('botcatcher')
+        if len(bot)>0:
+            raise forms.ValidationError('Bot has catched')
+    
+    def clean(self):
+        e=self.cleaned_data.get('email')
+        r=self.cleaned_data.get('reenter_email')
+
+        if e!=r:
+            raise forms.ValidationError('Emails Are Not matched')
